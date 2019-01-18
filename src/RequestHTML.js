17f44720d3413c3a725f5a3483ee2a879ffa42b9
Paul Vu <vudpaul@gmail.com>
2019-01-18 11:08:07 -0500

    Added logic to scrape links from html

--- src/RequestHTML.js
+++ src/RequestHTML.js
@@ -11,7 +11,7 @@ class RequestHTML {
 
   /**
    * Creates a promise
-   * @param {Object} options used as a parameter in the 'request' library 
+   * @param {Object} options used as a parameter in the 'request' library
    */
   __fetchPage(options) {
     return new Promise(function(resolve, reject) {
@@ -28,11 +28,11 @@ class RequestHTML {
   /**
    * Promise that returns the HTML content of a page.
    * Ex: instance.get("https://example.com").then( function (html) ... )
-   * @param {String} url of the page to fetch 
+   * @param {String} url of the page to fetch
    */
   get(url) {
     const options = { url: url, headers: this.headers };
-    return this.fetchPage(options).then(
+    return this.__fetchPage(options).then(
       function(response) {
         return response.body;
       },
@@ -40,5 +40,20 @@ class RequestHTML {
         console.log(err);
       }
     );
+  }
+
+  /**
+  * Getting all the absolute and relative links from HTML
+  * @param {String} html page to be crawled
+  * @return {Array} URLs grabbed from the html
+  */
+  getPaths(html) {
+    const urls = [];
+    const $ = cheerio.load(html);
+    const links = $('a');
+    $(links).each(function(i, link){
+      urls.push($(link).attr('href'));
+    });
+    return urls;
   }
 }
