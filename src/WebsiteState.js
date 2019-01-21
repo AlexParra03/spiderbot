52a30a5564b2c6886d651a1d42563b4eb70444a0
David Alejandro Parra <alex.user03@gmail.com>
2019-01-21 18:17:33 -0500

    PathTree logic added

--- src/WebsiteState.js
+++ src/WebsiteState.js
@@ -1,31 +1,57 @@
 /**
  * Data structure that holds a single website information
  */
-export class WebsiteState {
-    /**
-     * https://websitebuilders.com/how-to/web-at-a-glance/url-examples/
-     * @param {string} protocol Ex. http, https 
-     * @param {string} topLevelDomain Ex. .com, .edu
-     * @param {string} domainName 
-     */
-    constructor(protocol, topLevelDomain, domainName ) {
-        this.protocol = protocol;
-        this.topLevelDomain = topLevelDomain;
-        this.domainName = domainName;
-        this.pathRoot = new PathNode('');
-    }
+class WebsiteState {
+  /**
+   * https://websitebuilders.com/how-to/web-at-a-glance/url-examples/
+   * @param {string} protocol Ex. http, https
+   * @param {string} topLevelDomain Ex. .com, .edu
+   * @param {string} domainName
+   */
+  constructor(protocol, topLevelDomain, domainName) {
+    this.protocol = protocol;
+    this.topLevelDomain = topLevelDomain;
+    this.domainName = domainName;
+    this.pathTree = new PathTree();
+  }
 }
 
 /**
  * Tree structure used to store path routes
  */
-class PathNode {
-    constructor(name) {
-        this.name = name;
-        this.children = [];
-    }
+class PathTree {
+  constructor() {
+    this.root = new PathNode('');
+  }
 
-    addChild(childName) {
-        this.children.push(new PathNode(childName));
-    }
+  /**
+   * A path formated where each '/' should be before each file/folder (can't end with '/'). Ex. /a/b/c, /a/b
+   * @param {string} path should be a path starting with '/'
+   */
+  addPath(path) {
+    const paths = path.split('/').filter(elem => elem.trim() !== '');
+    let currentPathNode = this.root;
+    paths.forEach(function(currentPath) {
+      const childPathNode = currentPathNode.children.find(
+        childPath => childPath.name === currentPath
+      );
+      if (!childPathNode) {
+        const newPathNode = new PathNode(currentPath);
+        currentPathNode.children.push(newPathNode);
+        currentPathNode = newPathNode;
+      } else {
+        currentPathNode = childPathNode;
+      }
+    });
+  }
+}
+
+class PathNode {
+  constructor(name) {
+    this.name = name;
+    this.visited = false;
+    /** if have permissions to visit the path. null if we haven't checked */
+    this.allowed = null;
+    this.children = [];
+  }
 }
